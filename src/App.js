0c4672e549105b5fbf03dc7f80aa7cf8b97cd85d
anssirantanen <aparantanen@gmail.com>
2017-11-13 21:26:18 +0200

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import {MessageList} from './components/MessageList';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          <MessageList/>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/MessageList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export class MessageList extends React.Component {
+    render(){
+        return(
+            <div>List</div>
+        )
+    }
+}
+
