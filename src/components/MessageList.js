ca6ea5113981c739493e97e5f682650797173ebc
anssirantanen <aparantanen@gmail.com>
2017-11-13 22:09:55 +0200

    Added linter.

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 export class MessageList extends React.Component {
-    render(){
-        return(
-            <div>List</div>
-        )
-    }
+  render(){
+    return(
+      <div>List</div>
+    );
+  }
 }
 
